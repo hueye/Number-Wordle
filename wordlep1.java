c4e0c1baaabaebdbd64bd9ed10778071228a329a
hueye <114522370+hueye@users.noreply.github.com>
2022-12-13 14:46:52 -0500

    Add files via upload

--- wordlep1.java
+++ wordlep1.java
@@ -1,7 +1,6 @@
 //wordle
 import java.util.Random;
 import javax.lang.model.type.ArrayType;
-import java.awt.Color;
 import java.sql.Array;
 import java.util.Arrays;
 import java.io.BufferedReader;
@@ -24,10 +23,10 @@ class Wordle {
 
         answer.add(random.toString());
         answer = answer.split("");
-        System.out.print(answer);
+        //System.out.print(answer);
 
         //the game
-        System.out.print("Welcome to Number Wordle! Please enter 5 numbers with no spaces into the terminal:");
+        System.out.print("Welcome to Number Wordle! The rules are simple: you have six tries to guess a five digit number. Please enter 5 numbers with no spaces into the terminal:");
         
         //testing the answer
         for(int timesGuessed = 0; timesGuessed < 6; timesGuessed++){
@@ -35,12 +34,95 @@ class Wordle {
 
            guess = guess.split("");
            answerArray = answer.split("");
-
+//first number
         if(guess.split[0] = answerArray[0]){
             System.out.println(guess.split[0] + " is in the correct spot!");
+
+        }else if(guess.split[0] = answerArray[1]){
+            System.out.println(guess.split[0] + " is misplaced.");
+
+        }else if(guess.split[0] = answerArray[2]){
+            System.out.println(guess.split[0] + " is misplaced.");
+
+        }else if(guess.split[0] = answerArray[3]){
+            System.out.println(guess.split[0] + " is misplaced.");
+
+        }else if(guess.split[0] = answerArray[4]){
+            System.out.println(guess.split[0] + " is misplaced.");
+        }
+//second number
+        if(guess.split[1] = answerArray[1]){
+            System.out.println(guess.split[1] + " is in the correct spot!");
+
+        }else if(guess.split[1] = answerArray[0]){
+            System.out.println(guess.split[1] + " is misplaced.");
+
+        }else if(guess.split[1] = answerArray[2]){
+            System.out.println(guess.split[1] + " is misplaced.");
+
+        }else if(guess.split[1] = answerArray[3]){
+            System.out.println(guess.split[1] + " is misplaced.");
+
+        }else if(guess.split[1] = answerArray[4]){
+            System.out.println(guess.split[1] + " is misplaced.");
+        }
+//third number
+        if(guess.split[2] = answerArray[2]){
+            System.out.println(guess.split[2] + " is in the correct spot!");
+
+        }else if(guess.split[2] = answerArray[0]){
+            System.out.println(guess.split[2] + " is misplaced.");
+
+        }else if(guess.split[2] = answerArray[1]){
+            System.out.println(guess.split[2] + " is misplaced.");
+
+        }else if(guess.split[2] = answerArray[3]){
+            System.out.println(guess.split[2] + " is misplaced.");
+
+        }else if(guess.split[2] = answerArray[4]){
+            System.out.println(guess.split[2] + " is misplaced.");
+        }
+//fourth number
+        if(guess.split[3] = answerArray[3]){
+            System.out.println(guess.split[3] + " is in the correct spot!");
+
+        }else if(guess.split[3] = answerArray[0]){
+            System.out.println(guess.split[3] + " is misplaced.");
+
+        }else if(guess.split[3] = answerArray[1]){
+            System.out.println(guess.split[3] + " is misplaced.");
+
+        }else if(guess.split[3] = answerArray[2]){
+            System.out.println(guess.split[3] + " is misplaced.");
+
+        }else if(guess.split[3] = answerArray[4]){
+            System.out.println(guess.split[3] + " is misplaced.");
+        }
+//fifth number
+        if(guess.split[4] = answerArray[4]){
+            System.out.println(guess.split[4] + " is in the correct spot!");
+
+        }else if(guess.split[4] = answerArray[0]){
+            System.out.println(guess.split[4] + " is misplaced.");
+
+        }else if(guess.split[4] = answerArray[1]){
+            System.out.println(guess.split[4] + " is misplaced.");
+
+        }else if(guess.split[4] = answerArray[2]){
+            System.out.println(guess.split[4] + " is misplaced.");
+
+        }else if(guess.split[4] = answerArray[3]){
+            System.out.println(guess.split[4] + " is misplaced.");
         }
 
-            
+//win condition
+        if(guess = answerArray){
+            System.out.println("Congrats, you got the Number Wordle in " + timesGuessed + "tries!");
+        }
+//lose condition
+        if(timesGuessed = 6){
+            System.out.println("You're out of attempts! Nice try. Refresh to play again.");
+        }
         }
     }
 }
